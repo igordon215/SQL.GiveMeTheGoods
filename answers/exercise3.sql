4528d12015ffe741d18ba6a9f58d8e70b33b8d44
igordon215 <igordon215@gmail.com>
2024-08-11 14:40:21 -0400

    exercise3

--- answers/exercise3.sql
+++ answers/exercise3.sql
@@ -1,2 +1,3 @@
 <-- Select all from the Students table records where the first letter
 <-- of the City starts with anything from an "a" to an "f".
+SELECT * FROM Students WHERE City REGEXP '^[a-f]';
