30ff517d50605c6dc73944bfa21492c23141f08c
igordon215 <igordon215@gmail.com>
2024-08-11 14:43:07 -0400

    exercise5

--- answers/exercise5.sql
+++ answers/exercise5.sql
@@ -1,2 +1,3 @@
 <--Use the IN operator to select all the records from the Students table
 <-- where Country is either "Sint Maarten" or "Haiti".
+SELECT * FROM Students WHERE Country IN ('Sint Maarten', 'Haiti');
